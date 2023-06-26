658a1369ecc71f282b0778635363c90887efa942
Bryson Phillip <bkphillip@icloud.com>
2023-06-26 11:51:49 -0700

    Add pixel divs to container div

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,15 @@
 const container = document.querySelector('#container');
+const containerWidth = container.offsetWidth;
+
 const rows = columns = 16;
 const numberOfPixels = rows * columns;
+const pixelWidth = pixelHeight = containerWidth / rows
+
 for (let i = 0; i < numberOfPixels; i++) {
-  const pixel = document.createElement('div');
+  let pixel = document.createElement('div');
   container.append(pixel);
+
+  pixel.classList.add('pixel');
+  pixel.style.width =  `${pixelWidth}px`;
+  pixel.style.height =  `${pixelHeight}px`;
 }
