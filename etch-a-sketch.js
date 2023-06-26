7a2c1cba68a20067995a96cc5aae4fedf8bcd61e
Bryson Phillip <bkphillip@icloud.com>
2023-06-26 10:14:24 -0700

    Add code to fill container with total number of pixel divs

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,7 @@
 const container = document.querySelector('#container');
+const rows = columns = 16;
+const numberOfPixels = rows * columns;
+for (let i = 0; i < numberOfPixels; i++) {
+  const pixel = document.createElement('div');
+  container.append(pixel);
+}
