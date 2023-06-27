140aa38c0d3e41819d389fc5b4ac27cbe51e0dad
Bryson Phillip <bkphillip@icloud.com>
2023-06-27 10:06:18 -0700

    Add grid reset button with user prompt for new dims

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,17 +1,40 @@
+const resetButton = document.querySelector('#resetButton');
+resetButton.addEventListener('click', resetSketch)
+
 const container = document.querySelector('#container');
 const containerWidth = container.offsetWidth;
+const rows = 16;
 
-const rows = columns = 16;
-const numberOfPixels = rows * columns;
-const pixelWidth = pixelHeight = containerWidth / rows
+generateGrid(rows);  //Generate initial 16x16 grid
 
-for (let i = 0; i < numberOfPixels; i++) {
-  let pixel = document.createElement('div');
-  container.append(pixel);
 
-  pixel.classList.add('pixel');
-  pixel.style.width =  `${pixelWidth}px`;
-  pixel.style.height =  `${pixelHeight}px`;
+function resetSketch() {
+  const newRows = prompt("How many columns would you like for your new sketch?");  //Propt user for new dimensions of grid
+  const newNumberPixels = newRows * newRows;
 
-  pixel.addEventListener('mouseover', () => pixel.style.backgroundColor = 'black');
+  //Remove all previous pixel nodes
+  let child = container.lastElementChild; 
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+
+  generateGrid(newRows);    //Generate new grid
+}
+
+
+function generateGrid(rows) {
+  const numberOfPixels = rows * rows;
+  const pixelWidth = containerWidth / rows;
+
+  for (let i = 0; i < numberOfPixels; i++) {
+    let pixel = document.createElement('div');
+    container.append(pixel);
+  
+    pixel.classList.add('pixel');
+    pixel.style.width =  `${pixelWidth}px`;
+    pixel.style.height =  `${pixelWidth}px`;
+  
+    pixel.addEventListener('mouseover', () => pixel.style.backgroundColor = 'black');
+  }
 }
